2138a589e21cc5866910afb4ee1f7c54059f88c0
Michael Gysel <michael.gysel@unblu.com>
2025-12-04 10:00:12 +0100

    Day 4

---
+++ src/main/kotlin/Day04.kt
@@ -0,0 +1,54 @@
+fun main() {
+    solve("Example 1", 13) {
+        val grid = loadInput("example04-1.txt")
+        grid.findMoveableRolls().size
+    }
+
+    val grid = loadInput("day04.txt")
+    solve("Part 1", 1416) {
+        grid.findMoveableRolls().size
+    }
+
+    solve("Part 2", 9086) {
+        val newGrid = grid.toMutableMap()
+        var rollsRemoved = 0
+        do {
+            val rolls = newGrid.findMoveableRolls()
+            rolls.forEach { (coordinates, _) -> newGrid[coordinates] = '.' }
+            rollsRemoved += rolls.size
+        } while (rolls.isNotEmpty())
+        rollsRemoved
+    }
+}
+
+private fun loadInput(filename: String): Map<Coordinates, Char> {
+    val lines = InputData.readLines(filename)
+    return parseGrid(lines)
+}
+
+private fun parseGrid(lines: List<String>): Map<Coordinates, Char> {
+    val grid = mutableMapOf<Coordinates, Char>()
+    lines.forEachIndexed { y, line ->
+        line.forEachIndexed { x, c ->
+            grid[Coordinates(x, y)] = c
+        }
+    }
+    return grid
+}
+
+private fun Map<Coordinates, Char>.findMoveableRolls() = filter { (coordinates, char) ->
+    (char == '@') && (coordinates.neighbours().count { (this[it] ?: '.') == '@' } < 4)
+}
+
+data class Coordinates(val x: Int, val y: Int) {
+    fun neighbours() = listOf(
+        Coordinates(x + 1, y),
+        Coordinates(x + 1, y + 1),
+        Coordinates(x, y + 1),
+        Coordinates(x - 1, y + 1),
+        Coordinates(x - 1, y),
+        Coordinates(x - 1, y - 1),
+        Coordinates(x, y - 1),
+        Coordinates(x + 1, y - 1),
+    )
+}
